2bb96a9dfcba94f749565184ae265fd847653879
MYuriOli <140696135+MYuriOli@users.noreply.github.com>
2024-03-14 10:21:43 +0800

    signup connected(?) with express server ewan

--- Phase 2/yuri/testing phase/app.js
+++ Phase 2/yuri/testing phase/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
 
+const sampleStudents = require('./sampleStudents.js');
+const usersData = [];
+
+app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/signup', (req, res) => {
+  const { FirstName, LastName, email, password } = req.body;
+
+  const newUser = {
+    FirstName,
+    LastName,
+    email,
+    password
+  };
+
+  usersData.push(newUser);
+  console.log('New user added:', newUser)
+  res.redirect('3a-login-option.html');
+});
+
 
 app.listen(3000, () => {
     console.log(`Server is listening at http://localhost:${3000}`);
