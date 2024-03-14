2552d37ab126331b01671a1399b527fc537f3ba3
ErikRodr <139837317+ErikRodr@users.noreply.github.com>
2024-03-14 22:00:12 +0800

    Update script.js

--- Phase 2/erik/script.js
+++ Phase 2/erik/script.js
@@ -44,6 +44,17 @@ var sampleStudents = [
     }
 ];
 
+var seats = [
+    { seatNumber: 1, userEmail: 'john.doe@example.com', time: '0700 - 0730' },
+    { seatNumber: 2, userEmail: 'john.doe@example.com', time: '0730 - 0800' },
+    { seatNumber: 3, userEmail: 'jane.smith@example.com', time: '0730 - 0800' },
+    { seatNumber: 4, userEmail: 'jane.smith@example.com', time: '1300 - 1330' },
+    { seatNumber: 5, userEmail: 'bob.brown@example.com', time: '0700 - 0730' },
+    { seatNumber: 6, userEmail: 'emily.davis@example.com', time: '0730 - 0800' },
+    { seatNumber: 7, userEmail: 'emily.davis@example.com', time: '1300 - 1330' },
+
+];
+
 // Retrieve existing user data from session storage
 var userDataArray = JSON.parse(sessionStorage.getItem('userData')) || [];
 
